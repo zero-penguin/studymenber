d5c4cf480cbf1c0f969c22bf571d4ae6ec21af94
zero-penguin <arato1060@outlook.jp>
2023-08-27 13:45:29 +0900

    inpreve router

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,4 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'post_list.html', posts)
+    return render(request, 'post_list.html', {'posts':posts})
