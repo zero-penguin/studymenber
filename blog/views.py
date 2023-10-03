6608bd10d489a0aedc9a95d0fa2637aaef268ae9
zero-penguin <arato1060@outlook.jp>
2023-10-04 02:39:11 +0900

    fix detail

--- blog/views.py
+++ blog/views.py
@@ -12,7 +12,6 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
-from typing import Final
 
 #ログイン
 def Login(request):
